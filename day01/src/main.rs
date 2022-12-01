aa51e36f1c31526eced6dc79160ea424efafced0
bearsdotzone <bears@bears.zone>
2022-12-01 12:40:16 +0100

    finished

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,12 +1,39 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::str::FromStr;
 
 fn main() -> std::io::Result<()> {
     let mut file = File::open("input")?;
-    let mut buffer = [0; 10];
+    let mut buffer = String::from("");
+    let _n = file.read_to_string(&mut buffer)?;
 
-    // read up to 10 bytes
-    let n = file.read(&mut buffer[..])?;
-    print!("{}", n);
+    let mut max = 0;
+
+    let elves = buffer.split("\n\n");
+
+    let mut elf_iter = elves.into_iter();
+
+    let mut whole_elf = elf_iter.next();
+
+    while !whole_elf.is_none() {
+        let elf_nugs = whole_elf.unwrap().split("\n");
+
+        let out = elf_nugs.fold(0, |accum, item| {
+            let parsed_cal = i32::from_str(item);
+            match parsed_cal {
+                Ok(parsed_cal) => accum + parsed_cal,
+                Err(_) => accum,
+            }
+        });
+
+        if out >= max {
+            max = out;
+        }
+
+        whole_elf = elf_iter.next();
+    }
+
+    println!("{}", max);
+
     Ok(())
 }
